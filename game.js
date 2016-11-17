caad816f54bc3171bfd697c3ce403bcb2b56b544
Ahmoray Arana <bebegirl217@gmail.com>
2016-11-17 08:54:52 -0800

    Creating Food Animations

--- game.js
+++ game.js
@@ -1,9 +1,19 @@
 var disMinions = 40;
 document.addEventListener("keydown", function(e) {
   if (e.keyCode == 37) {
-   document.getElementById("black").setAttribute("x", batPaws - 25);
+   document.getElementById("black").setAttribute("x", disMinions - 25);
       disMinions = disMinions - 25;
     }
   if (e.keyCode == 39) {
-   document.getElementById("black").setAttribute("x", batPaws + 25);
+   document.getElementById("black").setAttribute("x", disMinions + 25);
     disMinions = disMinions + 25;
+  }
+    if (e.keyCode == 38) {
+     document.getElementById("black").setAttribute("y", disMinions + 15);
+      disMinions = disMinions + 15;
+    }
+      if (e.keyCode == 40) {
+       document.getElementById("black").setAttribute("y", disMinions - 15);
+        disMinions = disMinions - 15;
+      }
+  })
